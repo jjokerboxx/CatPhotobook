730a01e264aefd1f9209183b99510dc42b90599d
lee gihun <leegihun@leeui-MacBookAir-2.local>
2021-07-09 15:05:20 +0900

    async await

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-import request from "./api";
+"use strict";
+
+import request from "./api.js";
+
+const api = "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev/";
+
+let asd = await request(api)
+console.log(asd);
+
+// access to DOM elem
+let cat = document.querySelector(".cat");
+// create new DOM elem
+const $node = document.createElement('div');
+$node.setAttribute("class", "dog");
+$node.innerText= "dogo";
+// render(or attach) created DOM elem
+cat.appendChild($node);
