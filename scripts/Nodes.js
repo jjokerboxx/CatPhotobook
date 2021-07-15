52e8c1becd40444e7196601e70427310d4154297
lee gihun <leegihun@leeui-MacBookAir-2.local>
2021-07-15 17:13:53 +0900

    img tag but couldn't find the image api

--- scripts/Nodes.js
+++ scripts/Nodes.js
@@ -12,7 +12,9 @@ function Nodes($app, initialState){
     }
     this.render = () => {
         //state에는 node라는 Array-attribute가 있음
-        this.$target.innerHTML += this.state.map(node => `<li>${node.name}</li>`);
+        //나였으면 for문으로 했을 것을 map으로 매우 깔끔한 코드로 작성됨...!
+        this.$target.innerHTML += this.state.map(node => `<li>${node.name}<div>${node.type} <img src="${node.filePath}"></div></li>`);
+
     }
     
     //초기화하면서 화면 렌더링
